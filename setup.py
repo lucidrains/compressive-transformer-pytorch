6bc2d51840ba498997c481e9b288a7422acbcd17
Phil Wang <lucidrains@gmail.com>
2020-07-03 12:01:23 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'compressive-transformer-pytorch',
   packages = find_packages(exclude=['examples']),
-  version = '0.2.0',
+  version = '0.2.1',
   license='MIT',
   description = 'Implementation of Compressive Transformer in Pytorch',
   author = 'Phil Wang',
